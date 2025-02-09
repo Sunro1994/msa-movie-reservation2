dcba1bd4cf2210bcf2c6a48d14e7d2656cfeadc4
Sunro1994 <gwangjulsr@gmail.com>
2025-02-09 20:37:37 +0900

    initialCommit

---
+++ src/main/java/com/sparta_cloud/msa_movie_reservation/MsaMovieReservationApplication.java
@@ -0,0 +1,13 @@
+package com.sparta_cloud.msa_movie_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaMovieReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaMovieReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta_cloud/msa_movie_reservation/MsaMovieReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta_cloud.msa_movie_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaMovieReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
